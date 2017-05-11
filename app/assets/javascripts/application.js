108bd3204c1c09dcda481fc33e53eeea3dfa4a5a
Haoyu Liu <liuhaoyuca@gmail.com>
2017-05-11 21:19:43 +0000

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,40 @@ $(document).ready(function(){
     });
 
 });
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(document).ready(function(){
+    // on scroll, let the interval function know the user has scrolled
+    $(window).scroll(function(event){
+      didScroll = true;
+    });
+    // run hasScrolled() and reset didScroll status
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = st;
+    }
+});
