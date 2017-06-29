431beec6f5b2adb7850cebf7d322e75700f7bf0d
Haoyu Liu <liuhaoyuca@gmail.com>
2017-06-29 20:13:15 +0000

    fixing2

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ end
 
 group :production do
   gem 'pg', '0.18.4'
+  gem 'rmagick'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/migrate/20170605194851_add_info_to_publications.rb
+++ db/migrate/20170605194851_add_info_to_publications.rb
@@ -1,5 +1,5 @@
 class AddInfoToPublications < ActiveRecord::Migration[5.0]
   def change
-    add_column :publications, :time, :date
+    add_column :publications, :time, :integer
   end
 end
