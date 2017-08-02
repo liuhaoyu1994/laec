86c779abf4bb4b2fffc6a1c3c64a06c99d85d3e5
liuhaoyu1994 <liuhaoyuca@gmail.com>
2017-08-02 14:46:11 -0700

    fix facility

--- Gemfile
+++ Gemfile
@@ -35,7 +35,8 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.18.4'
+  gem 'pg'
+  gem 'unicorn'
   gem 'rmagick'
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
